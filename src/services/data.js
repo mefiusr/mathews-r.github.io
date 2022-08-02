6e68ac3620a918e4b6bde8e21fa76f065fff4fac
Mathews <mathewsrodrigues9@gmail.com>
2022-08-02 15:19:24 -0300

    removendo arquivos

--- src/services/data.js
+++ src/services/data.js
@@ -3,6 +3,7 @@ import Clima from '../images/clima.jpg';
 import Trunfo from '../images/trunfo.png';
 import Music from '../images/music.png';
 import Store from '../images/store.png';
+import clock from '../clockscript/relogio.html';
 
 const data = [
   {
@@ -38,7 +39,7 @@ const data = [
     name: 'Relógio JavaScript',
     description: 'JavaScript e manipulação de DOM',
     image: 'https://super.abril.com.br/wp-content/uploads/2011/01/em-que-hora-acontecem-mais-enfartes.png',
-    url: 'projects/clockscript/relogio.html',
+    url: clock,
   },
   {
     id: 'store',
