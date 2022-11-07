b3d3b1a0d307d0fcc55cb3e5892d472f9599f8a7
Mathews <mathewsrodrigues9@gmail.com>
2022-11-07 10:14:30 -0300

    ajustes no texto/responsividade

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -8,24 +8,25 @@ export default class About extends Component {
   render() {
     return (
       <section className="about" id="about">
-        
-      <div className="div-img-article">
-        <img className="img-article" src={Mathews} alt="Foto do Mathews" />
-      </div>
-      <div className="about-me">
-        <h1>SOBRE MIM</h1>
-        <h2>Barra Mansa, Rio de Janeiro</h2>
 
-        <p className="para">
-         Oi! Sou Técnico em Automação e atualmente estou em transição de carreira. <br />Estou me dedicando nos estudos para me tornar um desenvolvedor fullstack. <br />Em fev/2022 tomei a decisão de mudar de área e buscar novos desafios.<br />
-         Atualmente estou estudando Desenvolvimento Web na Trybe.
-      </p>
-      <div className="curriculo">
-        <img src={download} alt="Download do currículo" className="logoDownload"/>
-      <a download="CVMATHEWS.pdf" target="_blank" href="https://github.com/mathews-r/mathews-r.github.io/blob/master/src/assets/CV_MR_FRONT_PT.pdf" rel="noreferrer" >Currículo</a>
-      </div>
-      </div>
-  </section>
+        <div className="div-img-article">
+          <img className="img-article" src={Mathews} alt="Foto do Mathews" />
+        </div>
+        <div className="about-me">
+          <h1>SOBRE MIM</h1>
+          <h2>Barra Mansa, Rio de Janeiro</h2>
+
+          <p className="para">
+            Em transição de carreira, estou me dedicando nos estudos para me tornar um desenvolvedor fullstack. <br />Em fev/2022 tomei a decisão de mudar de área e buscar novos desafios.<br />
+            Atualmente estou estudando Desenvolvimento Web na Trybe.
+          </p>
+          <div className="curriculo">
+            <a download="CVMATHEWS.pdf" target="_blank" href="https://github.com/mathews-r/mathews-r.github.io/blob/master/src/assets/CV_MR_FRONT_PT.pdf" rel="noreferrer" >
+              <img src={download} alt="Download do currículo" className="logoDownload" />
+              Currículo</a>
+          </div>
+        </div>
+      </section>
 
     )
   }
